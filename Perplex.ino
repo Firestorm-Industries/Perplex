6793aaac9aec2ddcc72ded725dec9291b8b5689e
Robert <robpl@peter.com.au>
2023-10-28 17:21:10 +0800

    initial

---
+++ Perplex.ino
@@ -0,0 +1,16 @@
+#include <HardwareSerial.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(15, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(15, HIGH);
+  delay(500);
+  digitalWrite(15, LOW);
+  delay(100);
+  Serial.println(millis());
+}
